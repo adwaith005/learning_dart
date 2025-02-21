f353bb278369ffb1783e39cadb068605dd7a63b7
Adwaith <adwaithp435@gmail.com>
2025-02-21 10:47:36 +0530

    CONST&FINAL

--- bin/learningdart.dart
+++ bin/learningdart.dart
@@ -1,53 +1,11 @@
 void main() {
-  // Declaring variables
-  String name = 'Adwaith';
-  bool male = true;
-  int age = 21;
-  List<String> favColors = ['Blue', 'Black', 'White'];
-  List<dynamic> favNumbers = [1, 2, 6, 4, 5];
+  //A final variable can only be set once and it is initialized when accessed for the first time.
+  final String name = 'Adwaith';
+  // name = 'adsfasdf'; // Error: Final variables can't be assigned a value.
 
-  Map<String, dynamic> mydata = {
-    'name': name,
-    "sex": male,
-    'age': age,
-    'favColors': favColors,
-    'favnumber': favNumbers,
-    'stack': ['Dart', 'Flutter', 'Node', 'Nodeexpress'],
-  };
-
-  // Adding values to the list
-  favNumbers.add(6);
-  favNumbers.addAll([7, 8, 9]);
-
-  // Sorting the list
-  favNumbers.sort();
-
-  // Checking if list contains 6
-  print(favNumbers.contains(6));
-
-  // Filtering colors with case insensitivity
-  var val = favColors.where(
-    (element) =>
-        element.toLowerCase() == 'white' || element.toLowerCase() == 'black',
-  );
-
-  // Removing elements safely
-  favColors.remove('Black'); // Remove by value
-  if (favColors.isNotEmpty) {
-    favColors.removeAt(0); // Remove first element if list is not empty
-  }
-  if (favColors.isNotEmpty) {
-    favColors.removeLast(); // Remove last element if list is not empty
-  }
-
-  // Printing outputs
-  print(val);
-  print(mydata);
-  print('Keys: ${mydata.keys.toList()}');
-  print('Values: ${mydata.values.toList()}');
-  print('favColors length: ${favColors.length}');
-  print('favNumbers length: ${favNumbers.length}');
-  print('mydata length: ${mydata.length}');
-  print('Stack length: ${mydata['stack'].length}');
-  print('First stack item length: ${mydata['stack'][0].length}');
+  //A const variable is a compile-time constant. It is initialized during compilation.
+  const int age = 21;
+  // age = 234; // Error: Constant variables can't be assigned a value.
+  print(age);
+  print(name);
 }
