5920eb09f13cd78ed8296c423467b4d262c54f7d
Adwaith <adwaithp435@gmail.com>
2025-02-21 11:11:05 +0530

    If-else & else-if conditon

--- bin/learningdart.dart
+++ bin/learningdart.dart
@@ -1,19 +1,16 @@
-class A {
-  static String message = "Hello from A!";
-
-  static void greet() {
-    print("Greetings from A!");
-  }
-}
-
-class B {
-  void showMessage() {
-    print(A.message); // Accessing static variable from class A
-    A.greet(); // Calling static method from class A
-  }
-}
-
 void main() {
-  B obj = B();
-  obj.showMessage();
+  int age = 18;
+  print(vote(age));
+}
+
+String vote(int age) {
+  if (age >= 18) {
+    return "YOU CAN VOTE";
+  } else if (age >= 18 && age < 100) {
+    return "YOU CAN'T VOTE";
+  } else if (age <= 0) {
+    return "You are not born ";
+  } else {
+    return "invalid age";
+  }
 }
