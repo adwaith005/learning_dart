36fcbca61e8551ed2a1f6eb1c3a2182ad68e91e3
Adwaith <adwaithp435@gmail.com>
2025-02-24 21:39:09 +0530

    Default Parameter

--- bin/learningdart.dart
+++ bin/learningdart.dart
@@ -1,17 +1,8 @@
 void main() {
-  // Named Parameters & required parameters
-  // Named parameters are enclosed in curly braces {} and are optional
-  // Required parameters are marked with the required keyword
-  // Named parameters are used to make the code more readable and to avoid confusion
-  // Named parameters are used to pass the values to the function in any order
-  // Named parameters are used to pass the values to the function by specifying the parameter name
-  // Named parameters are used to pass the values to the function by specifying the parameter name
-  Map userdetails = userMap(
-    name: 'John',
-    age: 25,
-    gender: 'male',
-    classno: 101,
-  );
+  // default parameters
+  // default parameters are used when the value is not passed to the function
+
+  Map userdetails = userMap(name: 'John', age: 25, gender: 'male');
   print(userdetails);
 }
 
@@ -19,7 +10,7 @@ Map<String, dynamic> userMap({
   required String name,
   required int age,
   required String gender,
-  required int classno,
+  int classno = 0,
 }) {
   return {'name': name, 'age': age, 'gender': gender, 'classno': classno};
 }
