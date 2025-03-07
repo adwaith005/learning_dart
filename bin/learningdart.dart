2ab456601cfcfa4731c573156ce2591c0ccd0b24
Adwaith <adwaithp435@gmail.com>
2025-03-07 13:30:01 +0530

    Inheritance Code Examples

--- bin/learningdart.dart
+++ bin/learningdart.dart
@@ -1,36 +1,24 @@
-// Constructor is the first method that runs when the object is created.
-class Mathamatics {
-  int n1 = 0;
-  int n2 = 10;
-  Mathamatics(int n1, int n2) {
-    this.n1 = n1;
-    this.n2 = n2;
-    print("object is created");
+class Electronics {
+  double height = 50;
+  double width = 100;
+  double thickness = 10;
+  watch() {
+    print('Electronics items are being watched');
   }
-  Mathamatics.hellow() {
-    print('named constructor');
-  }
-  int addition() {
-    return this.n1 + this.n2;
+}
+
+class Mobile extends Electronics {
+  void playclass() {
+    print('Mobile is being played');
   }
 
-  int subtraction() {
-    return this.n1 - this.n2;
-  }
-
-  int multiplication() {
-    return this.n1 * this.n2;
-  }
-
-  double division() {
-    return this.n1 / this.n2;
+  void callcontact() {
+    print('Mobile is being called');
+    print(height);
   }
 }
 
 void main() {
-  Mathamatics maths = Mathamatics(112, 12);
-  print(maths.addition());
-  print(maths.subtraction());
-  print(maths.multiplication());
-  print(maths.division());
+  Mobile mobile = Mobile();
+  print(mobile.watch());
 }
